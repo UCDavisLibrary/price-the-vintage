aefebc0393b5cb48d1f0d5f41f75f5d281152c6c
Justin Merz <jrmerz@gmail.com>
2017-04-14 15:00:28 -0700

    bower element cleanup, performance and a11y improvements working on lighthouse score

--- build/build-dist.sh
+++ build/build-dist.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 # Simple build script for now
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 
 DIST=$DIR/dist
 APP=$DIR/public
+
+# first, build non-source map version of app
+browserify -v -t bulkify $DIR/build/glob.js -o $DIR/public/js/lib.js
 
 CP=("$APP/js" "$APP/images" "$APP/silent-login.html" "$APP/index.html" "$APP/offline.html" "$APP/sw.js" "$APP/manifest.json")
 
