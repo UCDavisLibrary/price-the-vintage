1a4dd9a8bca4443715e34af8253a8ad424511170
Justin Merz <jrmerz@gmail.com>
2017-07-16 10:10:13 -0700

    switching to cork-app-utils

--- build/glob.js
+++ build/glob.js
@@ -1,8 +1,10 @@
 var bulk = require('bulk-require');
 var path = require('path');
 
+require('../lib/init')();
 var App = bulk(path.join(__dirname, '..', 'lib'), [ '**/*.js']);
+
 window.App = App;
-window.EventBus = App.eventBus;
+window.EventBus = require('cork-app-utils').EventBus;
 window.BufferedEvents = App.utils.bufferedEvents;
 window.AnimateScroll = App.utils.AnimateScroll;
